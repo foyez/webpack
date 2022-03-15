8a8aa254938a85767ec4cdce7d995880e85f2032
foyez <foyezar@gmail.com>
2022-03-16 01:26:42 +0600

    add css module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import { add } from "@/utils/add";
+import styles from "@/styles/style.module.scss";
 
 console.log(add(1, 6));
+
+const rootEl = document.getElementById("root");
+
+rootEl.innerHTML = `<p class="${styles.text}">CSS Modules Webpack</p>`;
